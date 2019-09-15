510a668072eb0cbfda20e0b30bf19adba77cee0a
Edwin Chen <edwinchen85@gmail.com>
2019-09-15 20:52:28 +0800

    Add card web component

--- js/main.js
+++ js/main.js
@@ -1,15 +1,2 @@
-document.querySelector('.icon-edit').addEventListener('click', e => {
-  document.querySelector('.card-title').setAttribute('contenteditable', true);
-  document.querySelector('.card-title').focus();
-  document.querySelector('.card-title').addEventListener('keypress', e => {
-    if (e.keyCode === 13) { // enter key is pressed
-      e.target.blur();
-      e.target.removeAttribute('contenteditable');
-      return false;
-    }
-  });
-});
-document.querySelector('.icon-caret').addEventListener('click', e => {
-  document.querySelector('.icon-caret').classList.toggle('rotate');
-  document.querySelector('.card-desc').classList.toggle('show');
-});
+(function() {
+})();
